c5a0a09620b91e1ef6de8298ebd908c66410a129
Michał Lubowicz <lubowicz.michal@gmail.com>
2020-06-25 00:36:47 +0200

    Initial Commit

---
+++ Wisielec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wisielec
+//
+//  Created by Michał Lubowicz on 25/06/2020.
+//  Copyright © 2020 Michał Lubowicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
